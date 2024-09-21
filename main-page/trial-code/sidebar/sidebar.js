d1a2d61d87f2ee2f6a047b870aec16b51e6863a0
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-09-21 23:36:04 +0530

    sidebar tried to add in new way

---
+++ main-page/trial-code/sidebar/sidebar.js
@@ -0,0 +1,8 @@
+const sideBar = document.getElementById('sideBar');
+const h2 = document.createElement('h2');
+h2.textContent = 'My To Do List';
+const div = document.createElement('div');
+div.id = 'displayUsername';
+
+sideBar.appendChild(h2);
+sideBar.appendChild(div);
