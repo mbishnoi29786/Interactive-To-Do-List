843eddc88ba18268418df8b5356653d2a566857b
mbishnoi29786 <mbishnoi29786@gmail.com>
2024-10-01 23:02:19 +0530

    corrected some css and tried to add add task functionality using prompt

--- main-page/trial-code/Dialogue_boxes/addTask_dialogBox.js
+++ main-page/trial-code/Dialogue_boxes/addTask_dialogBox.js
@@ -9,12 +9,51 @@ export function createAddTaskDialog(USER) {
     addTaskInput.className = "addTask-input";
 
     let searchSpan = document.createElement('div');
-    searchSpan.innerHTML = '<p>Search</p>'
+    searchSpan.innerHTML = '<p>Add</p>'
     searchSpan.className = 'search-span';
+
+    searchSpan.addEventListener('click', ()=> addTask(USER));
 
     searchIconDiv.appendChild(searchIcon);
     searchDiv.appendChild(searchIconDiv)
     searchDiv.appendChild(searchSpan);
 
     return addTaskDiv;
+}
+
+function addTask(USER)
+{
+    let userLists = JSON.parse(localStorage.getItem(USER)) || []
+    let taskExist = list.tasks.find(tasks => tasks.taskName === input.value.trim());
+        let deadlineValue = new Date(deadlineInput.value);
+        let now = new Date();
+
+        if (input.value.trim() === '') 
+        {
+            alert("Write a task!");
+        } 
+        else if (taskExist) 
+        {
+            alert("Task Already Exists!!");
+        }
+        else if (isNaN(deadlineValue))
+        {
+            alert("Enter a dealine!!");
+        } 
+        else if (deadlineValue <= now) 
+        {
+            alert("Deadline must be in the future!");
+        } 
+        else 
+        {
+            list.tasks.push({
+                taskName: input.value.trim(),
+                deadline: deadlineInput.value,
+                completed: false
+            });
+            updateListsInStorage(list.name, list.tasks);
+            displayTasks(list.tasks, ul, list.name);
+            input.value = '';
+            deadlineInput.value = '';
+        }
 }
